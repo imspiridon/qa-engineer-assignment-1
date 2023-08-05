02e7aab1a2667faceecacf5f3da9ebd4897e953b
Spiridon <imspiridon@gmail.com>
2023-08-05 11:40:49 +0900

    update

--- testAssignment.spec.js
+++ testAssignment.spec.js
@@ -1,13 +1,39 @@
-const request = require('supertest');
+const { BigInteger } = require('bignumber');
+const request = require('supertest')('https://petstore.swagger.io/v2');
 const expect = require('chai').expect;
-const baseUrl = 'petstore.swagger.io/v2'
 
 describe('First test', () => {
-    it('Can find pet by status', (done) => {
-        request(baseUrl)
-            .get('/findByStatus?status=available')
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .done();
+    // it('Can find pet by status', () => {
+    //     request(baseUrl)
+    //         .get('/pet/findByStatus?status=available')
+    //         .expect(200)
+    //         .expect('Content-Type', /text\/html/)
+    //         .expect(res.body)
+    //         .end(function(err, res) {
+    //             if (err) throw err;
+    //             console.log(res.body)
+
+    //         })
+    // })
+
+    it('/GET Can find pets by status', async () => {
+        // Verifies that searching pets by status returns correct status code and is not empty
+        const response = await request
+            .get('/pet/findByStatus?status=available');
+
+        expect(response.status).to.equal(200);
+        expect(response.body).to.be.not.empty;
+            })
+        
+    it('/GET Can find pets by ID', async () => {
+        // Verifies that searching pets by ID returns correct results (checks response properties: name, category name, first tag name)
+        const petId = 101;
+        const response = await request
+            .get(`/pet/${petId}`);
+        
+        expect(response.body).to.have.property('name').equals('doggie');
+        expect(response.body).to.have.nested.property('category.name').equals('sheru');
+        expect(response.body).to.have.nested.property('tags[0].name').equals('sheru');
     })
 });
+
